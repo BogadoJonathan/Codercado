fc130aa24cba757693cfe785436d6f8f56b64f9a
BogadoJonathan <Bogadojonathan@outlook.com>
2021-12-20 23:29:11 -0300

    desafio complementario de eventos

--- main.js
+++ main.js
@@ -1,18 +1,17 @@
 //OBJETOS
 class Producto{
-    constructor(id,marca, precio, tipo){
+    constructor(id,descripcion, precio, image){
         this.id = id;
-        this.marca = marca;
+        this.descripcion = descripcion;
         this.precio = precio;
-        this.tipo = tipo;
+        this.image = image;
     }
 
     imprimir(){
-        return `${this.tipo} - ${this.marca} $${this.precio}`;
+        return `${this.descripcion} $${this.precio}`;
     }
 
     sumarAlCarrito(){
-        alert(`$${this.precio} fueron sumados a tu carrito`)
         total+=this.precio
     }
 }
@@ -20,36 +19,71 @@ class Producto{
 //VARIABLES
 let total = 0
 const productos = []
-productos.push(new Producto(1,"Huawei",75000,"celular"));
-productos.push(new Producto(2,"Samsung",95000,"celular"));
-productos.push(new Producto(3,"Motorola",80000,"celular"));
-productos.push(new Producto(4,"Xiomi",70000,"celular"));
-productos.push(new Producto(5,"Iphone",120000,"celular"));
+productos.push(new Producto(1,"Antifaz para dormir x2",400,"antifaz.jpg"));
+productos.push(new Producto(2,"Botella acero inoxidable",700,"botella.jpg"));
+productos.push(new Producto(3,"Buzo",1000,"buzo.jpg"));
+productos.push(new Producto(4,"funda para iphone",800,"funda.jpg"));
+productos.push(new Producto(5,"gorro",500,"gorro.jpeg"));
+productos.push(new Producto(6,"mochila",1000,"mochila.jpg"));
+productos.push(new Producto(7,"remera manga corta",500,"remera.jpg"));
+productos.push(new Producto(8,"taza",300,"taza.jpg"));
 
+
+let listProductos = document.getElementById("listProductos")
+let listCarrito = document.getElementById("listCarrito")
+
+const carrito = []
 let opcion = 0
 let continuar = true
 
 //IMPRIMIR PRODUCTOS EN EL DOM
 function imprimirEnDOM(){
-
-    let listProductos = document.getElementById("listProductos")
-    //listProductos.removeChild()
     listProductos.innerHTML=``
-    productos.forEach(productosArray =>{
+    productos.forEach((productosArray) =>{
         
         listProductos.innerHTML+=`
-        <li class="itemProductos">${productosArray.imprimir()} <button id="btnItem">+</button></li><br>
+        <div class="card" style="width: 18rem;">
+            <img src="img/${productosArray.image}" class="card-img-top" alt="...">
+            <div class="card-body">
+                <h5 class="card-title">${productosArray.descripcion}</h5>
+                <p class="card-text">$${productosArray.precio}</p>
+                <a href="#" id="boton${productosArray.id}" class="btn btn-primary">Agregar +</a>
+            </div>
+        </div>
         `
     })
+
+    productos.forEach((productosArray,indice) =>{
+        document.getElementById(`boton${productosArray.id}`).addEventListener('click',()=>{
+            carrito.push(productos[indice])
+            imprimirCarrito()
+        })
+    })
+    
 }
 
-let btnMarca =  document.getElementById("ordenMarca")
+function imprimirCarrito(){
+    listCarrito.innerHTML=``
+    listCarrito.innerHTML=`<ul class="list-group">`
+    carrito.forEach((carritoArray)=>{
+        listCarrito.innerHTML+=`
+        <li class="list-group-item">${carritoArray.imprimir()}</li>
+        `
+        carritoArray.sumarAlCarrito()
+    })
+    listCarrito.innerHTML+=`
+    </ul>
+    <h2>Tu total es: $${total}</h2>
+    `
+}
+
+let btnDescripcion =  document.getElementById("ordenDescripcion")
 let btnPrecio =  document.getElementById("ordenPrecio")
 
 imprimirEnDOM()
 
-btnMarca.addEventListener('click',()=>{
-    ordenarPorMarca()
+btnDescripcion.addEventListener('click',()=>{
+    ordenarPorDescripcion()
     imprimirEnDOM()
 })
 
@@ -59,31 +93,16 @@ btnPrecio.addEventListener('click',()=>{
 })
 
 
-// boton.addEventListener('clic',() =>{
-//     //codigo
-// })
-
-// boton.onclic = () =>{
-//     //codigo
-// }
-
-// for(var i=0; i < productos.length;i++){
-//     let item = document.createElement("li")
-//     item.id=`${productos[i].marca}`
-//     item.className = "itemProductos"
-//     item.innerText += `${productos[i].imprimir()}`
-//     listProductos.appendChild(item)
-// }
 
 function resultado(){
     alert(`El total de sus productos es ${total}`)
 }
 
-function ordenarPorMarca(){
+function ordenarPorDescripcion(){
     productos.sort((o1,o2) =>{
-        if(o1.marca < o2.marca){
+        if(o1.descripcion < o2.descripcion){
             return -1;
-        }else if(o1.marca > o2.marca){
+        }else if(o1.descripcion > o2.descripcion){
             return 1;
         } else {
             return 0;
@@ -103,11 +122,14 @@ function ordenarPorPrecio(){
     });
 }
 
+
+
+
 // do{     
 //     opcion = parseInt(prompt(`
 //     Ingresa los productos que deseas llevar: 
 //     6: Ordenar por precio
-//     7: Ordenar por marca`))
+//     7: Ordenar por descripcion`))
 
 //     if(opcion<1 || opcion>7){
 //         alert("Debe ingresar un numero valido")
@@ -134,7 +156,7 @@ function ordenarPorPrecio(){
 //             ordenarPorPrecio();
 //             continue;
 //         case 7:
-//             ordenarPorMarca();
+//             ordenarPordescripcion();
 //             continue;
 
 //     }
